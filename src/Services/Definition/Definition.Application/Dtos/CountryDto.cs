f5482cde4cbdd526aa9b69ec9e2641bc061e0f83
Yoldaş Çevik <cevikyoldas@gmail.com>
2020-10-16 20:16:30 +0300

    [bugfix] change id property type to string

--- src/Services/Definition/Definition.Application/Dtos/CountryDto.cs
+++ src/Services/Definition/Definition.Application/Dtos/CountryDto.cs
@@ -5,7 +5,7 @@ namespace Definition.Application.Dtos
     [Serializable]
     public class CountryDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Iso3 { get; set; }
         public string Iso2 { get; set; }

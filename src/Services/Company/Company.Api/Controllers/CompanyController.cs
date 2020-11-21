6f4725b1d2ed9f662f1ae8ce286336fc652ab6bf
Yoldaş Çevik <cevikyoldas@gmail.com>
2020-11-21 20:28:22 +0300

    seq logging is added

--- src/Services/Company/Company.Api/Controllers/CompanyController.cs
+++ src/Services/Company/Company.Api/Controllers/CompanyController.cs
@@ -1,9 +1,23 @@
+using System;
 using Company.Api.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Company.Api.Controllers
 {
     public class CompanyController: CompanyApiController
     {
+        private readonly ILogger<CompanyController> _logger;
 
+        public CompanyController(ILogger<CompanyController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("company api created!");
+        }
+
+        public IActionResult Test()
+        {
+            throw new NotImplementedException("henuz hazır değil!");
+        }
     }
 }
--- src/Services/Company/Company.Api/Program.cs
+++ src/Services/Company/Company.Api/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.IO;
+using Logging;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Company.Api
 {
@@ -7,11 +12,44 @@ namespace Company.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            Log.Logger = CareerSerilogLoggerFactory.CreateSerilogLogger(configuration);
+
+            try
+            {
+                Log.Information("Application starting up...");
+
+                CreateHostBuilder(args).Build() .Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The application failed to start correctly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        
+        private static IConfiguration GetConfiguration()
+        {
+            string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
     }
 }

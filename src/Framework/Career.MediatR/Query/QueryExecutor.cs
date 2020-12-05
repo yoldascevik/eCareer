4579bf0f95f2c781d0efee211cc0ea61288f4a0a
Yoldaş Çevik <cevikyoldas@gmail.com>
2020-12-05 13:46:46 +0300

    Query & Command executor are added

---
+++ src/Framework/Career.MediatR/Query/QueryExecutor.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Career.IoC;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Career.MediatR.Query
+{
+    public static class QueryExecutor
+    {
+        public static async Task<TResult> Execute<TResult>(IQuery<TResult> query)
+        {
+            using (var scope = DIResolver.CreateScope())
+            {
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                return await mediator.Send(query);
+            }
+        }
+    }
+}

478c1b41cb31508a9a1eae44d0fd6b1dee84d2ed
Yoldaş Çevik <cevikyoldas@gmail.com>
2020-12-05 21:37:59 +0300

    logical specifications

---
+++ src/Framework/Career.Domain/Specifications/Logical/And.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+using Career.Exceptions;
+
+namespace Career.Domain.Specifications.Logical
+{
+    public class And<T> : Specification<T>
+    {
+        private readonly ISpecification<T> _left;
+        private readonly ISpecification<T> _right;
+
+        public And(ISpecification<T> left, ISpecification<T> right)
+        {
+            Check.NotNull(left, nameof(left));
+            Check.NotNull(right, nameof(right));
+
+            _left = left;
+            _right = right;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            ParameterExpression objParam = Expression.Parameter(typeof(T), "obj");
+
+            var newExpr = Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(
+                    Expression.Invoke(_left.ToExpression(), objParam),
+                    Expression.Invoke(_right.ToExpression(), objParam)
+                ),
+                objParam
+            );
+
+            return newExpr;
+        }
+    }
+}
---
+++ src/Framework/Career.Domain/Specifications/SpecificationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Career.Domain.Specifications.Logical;
+
+namespace Career.Domain.Specifications
+{
+    public static class SpecificationExtensions
+    {
+        public static ISpecification<T> And<T>(this ISpecification<T> left, ISpecification<T> right)
+        {
+            return new And<T>(left, right);
+        }
+
+        public static ISpecification<T> Or<T>(this ISpecification<T> left, ISpecification<T> right)
+        {
+            return new Or<T>(left, right);
+        }
+
+        public static ISpecification<T> Not<T>(this ISpecification<T> inner)
+        {
+            return new Not<T>(inner);
+        }
+    }
+}

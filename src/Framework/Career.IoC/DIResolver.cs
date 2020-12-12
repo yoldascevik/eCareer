be667530c6f41ee5dc8450319b92e5d302ef01c1
Yoldaş Çevik <cevikyoldas@gmail.com>
2020-12-12 23:58:55 +0300

    unitofwork atomic transaction attribute is added

--- src/Framework/Career.IoC/DIResolver.cs
+++ src/Framework/Career.IoC/DIResolver.cs
@@ -7,6 +7,17 @@ namespace Career.IoC
     {
         private static IServiceProvider _serviceProvider;
 
+        public static IServiceProvider ServiceProvider
+        {
+            get
+            {
+                if (_serviceProvider == null)
+                    throw new ArgumentException("ServiceProvider not set for DIResolver!");   
+
+                return _serviceProvider;
+            }
+        }
+
         public static void SetProvider(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
@@ -14,10 +25,7 @@ namespace Career.IoC
 
         public static IServiceScope CreateScope()
         {
-            if (_serviceProvider == null)
-                throw new ArgumentException("ServiceProvider not set for DIResolver!");
-            
-            return _serviceProvider.CreateScope();
+            return ServiceProvider.CreateScope();
         }
     }
 }

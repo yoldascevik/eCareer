09d5fe508a314ee2090a0a81091709d479bcbdc1
Yoldaş Çevik <cevikyoldas@gmail.com>
2021-06-27 16:27:15 +0300

    bug fix

--- src/Services/CV/CV.Infrastructure/Repositories/CVRepository.cs
+++ src/Services/CV/CV.Infrastructure/Repositories/CVRepository.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Career.EventHub;
 using Career.Mongo.Context;
 using Career.Mongo.Repository;
 using CurriculumVitae.Core.Entities;
 using CurriculumVitae.Core.Repositories;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace CurriculumVitae.Infrastructure.Repositories
@@ -18,19 +21,34 @@ namespace CurriculumVitae.Infrastructure.Repositories
         public async Task UpdateAllDisabilityTypeNamesInCV(DisabilityType disabilityType)
         {
             var filter = Builders<CV>.Filter.ElemMatch(cv => cv.PersonalInfo.Disabilities, Builders<Disability>.Filter.Eq(x => x.Type.Id, disabilityType.Id));
-            var update = Builders<CV>.Update.Set(cv => ((Disability[]) cv.PersonalInfo.Disabilities)[-1].Type.Name, disabilityType.Name);
+            var update = Builders<CV>.Update.Set("PersonalInfo.Disabilities.$[d].Type.Name", disabilityType.Name);
+            var updateOptions = new UpdateOptions()
+            {
+                ArrayFilters = new []
+                {
+                    new BsonDocumentArrayFilterDefinition<BsonDocument>(new BsonDocument("d.Type._id", new ObjectId(disabilityType.Id)))
+                }
+            };
 
-            await Collection.UpdateManyAsync(filter, update);
+            await Collection.UpdateManyAsync(filter, update, updateOptions);
         }
 
         public async Task UpdateAllSocialProfileTypesInCV(SocialProfileType socialProfileType)
         {
             var filter = Builders<CV>.Filter.ElemMatch(cv => cv.SocialProfiles, Builders<SocialProfile>.Filter.Eq(x => x.Type.Id, socialProfileType.Id));
             var update = Builders<CV>.Update
-                .Set(cv => ((SocialProfile[]) cv.SocialProfiles)[-1].Type.Name, socialProfileType.Name)
-                .Set(cv => ((SocialProfile[]) cv.SocialProfiles)[-1].Type.ProfileUrlPrefix, socialProfileType.ProfileUrlPrefix);
+                .Set("SocialProfiles.$[s].Type.Name", socialProfileType.Name)
+                .Set("SocialProfiles.$[s].Type.ProfileUrlPrefix", socialProfileType.ProfileUrlPrefix);
             
-            await Collection.UpdateManyAsync(filter, update);
+            var updateOptions = new UpdateOptions()
+            {
+                ArrayFilters = new []
+                {
+                    new BsonDocumentArrayFilterDefinition<BsonDocument>(new BsonDocument("s.Type._id", new ObjectId(socialProfileType.Id)))
+                }
+            };
+            
+            await Collection.UpdateManyAsync(filter, update, updateOptions);
         }
     }
 }

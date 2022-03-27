90ac95c8fb69c22f6269d16d8f83f5d8e86cff4f
Yoldaş Çevik <cevikyoldas@gmail.com>
2022-03-27 23:54:46 +0300

    added company admin role to test user

--- src/Services/Identity/Career.Identity/Quickstart/TestUsers.cs
+++ src/Services/Identity/Career.Identity/Quickstart/TestUsers.cs
@@ -21,7 +21,8 @@ public class TestUsers
                 new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
                 new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
                 new Claim(JwtClaimTypes.WebSite, "https://alice.com"),
-                new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json),
+                new Claim(JwtClaimTypes.Role,"CompanyAdmin")
             }
         },
         new TestUser{SubjectId = "88421113", Username = "bob", Password = "bob", 
